4128b1475b17e0e830074843464bfb2cf9337833
Nilah Ravi Nair <nilahnair.27@gmail.com>
2022-09-09 10:39:42 +0200

    Update LARa_dataset_preprocessing.py

--- LARa_dataset_preprocessing.py
+++ LARa_dataset_preprocessing.py
@@ -875,22 +875,22 @@ def create_dataset(half=True):
     val_ids = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10", "S11", "S12", "S13", "S14", "S15", "S16"]
     test_ids = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10", "S11", "S12", "S13", "S14", "S15", "S16"]
     '''
-    
-    train_ids = ["S06", "S07", "S10", "S11"]
-    val_ids = ["S06", "S07", "S10", "S11"]
 
-    test_ids = ["S01", "S02", "S03", "S04", "S05", "S08", "S09", "S12", "S13", "S14"]
+    train_ids = ["S05", "S06", "S08", "S10"]
+    val_ids = ["S05", "S06", "S08", "S10"]
+
+    test_ids = ["S01", "S02", "S03", "S04", "S07", "S09", "S11", "S12", "S13", "S14"]
     
     #general_statistics(train_ids)
 
     if half:
         "Path to the segmented sequences"
-        base_directory = '/data/nnair/datasetbias/prepros/exp23/'
+        base_directory = '/data/nnair/datasetbias/prepros/exp24/'
         sliding_window_length = 100
         sliding_window_step = 12
     else:
         "Path to the segmented sequences"
-        base_directory = '/data/nnair/datasetbias/prepros/exp23/'
+        base_directory = '/data/nnair/datasetbias/prepros/exp24/'
         sliding_window_length = 200
         sliding_window_step = 25
 
