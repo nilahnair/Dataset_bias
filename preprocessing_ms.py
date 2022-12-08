8539fe954efc94a7296bdb9a9b9e9329d181cd68
Nilah Ravi Nair <nilahnair.27@gmail.com>
2022-12-08 21:16:40 +0100

    Update preprocessing_ms.py

--- preprocessing_ms.py
+++ preprocessing_ms.py
@@ -447,10 +447,10 @@ if __name__ == '__main__':
     print("[INFO] -- Selected activites: "+str(act_labels))    
     trial_codes = [TRIAL_CODES[act] for act in act_labels]
     
-    base_directory = '/data/nnair/datasetbias/motionsense/prepros/exp9/'
+    base_directory = '/data/nnair/datasetbias/motionsense/prepros/exp10/'
     
-    sel_subjects_train=[3, 5, 14, 24]
-    sel_subjects_test=[1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+    sel_subjects_train=[5, 12, 14, 23]
+    sel_subjects_test=[1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 13, 15, 16, 17, 18, 19, 20, 21, 22, 24]
     
     creat_time_series(dt_list, act_labels, trial_codes, base_directory=base_directory, subjects=sel_subjects_train, mode="raw", labeled=True, usage_modus='trainval')
     creat_time_series(dt_list, act_labels, trial_codes, base_directory=base_directory, subjects=sel_subjects_test, mode="raw", labeled=True, usage_modus='test')
